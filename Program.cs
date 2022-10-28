3c5559ab1fdc4ee0a3330c1ed5a8349fbc929feb
gloriaghe <gloghe@yahoo.it>
2022-10-28 16:16:32 +0200

    fibonacci

--- Program.cs
+++ Program.cs
@@ -51,14 +51,22 @@ Console.WriteLine(fattorialeRicorsivo(5));
 
 int fibonacciRicorsivo(int numero)
 {
-    if (numero > 0)
-        return fibonacciRicorsivo(numero - 1) * numero;
+    if (numero > 2)
+    {
+        int numeroMenoUno = fibonacciRicorsivo(numero - 1);
+        int numeroMenoDue = fibonacciRicorsivo(numero - 2);
+
+        return numeroMenoUno + numeroMenoDue;
+
+    }
+    else if(numero == 0)
+        return 0;
     else
         return 1;
 
 }
 
-Console.WriteLine(fibonacciRicorsivo(5));
+Console.WriteLine(fibonacciRicorsivo(10));
 
 
 
